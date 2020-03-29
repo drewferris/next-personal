89771f82a657c93a93e1d18acc0422dbf4290712
Drew Ferris <c_dferris@groupon.com>
2020-03-29 10:06:03 -0700

    added global styling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
-import Layout from '../components/MyLayout';
-import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
+import Layout from "../components/MyLayout";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+
+const PostLink = ({ post }) => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${post.id}`}>
+      <a>{post.title}</a>
+    </Link>
+  </li>
+);
 
 const Index = props => (
   <Layout>
@@ -14,11 +22,30 @@ const Index = props => (
         </li>
       ))}
     </ul>
+    <style jsx>{`
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 );
 
 Index.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
   const data = await res.json();
 
   console.log(`Show data fetched. Count: ${data.length}`);
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,11 +1,30 @@
-import Layout from '../../components/MyLayout';
-import fetch from 'isomorphic-unfetch';
+import Layout from "../../components/MyLayout";
+import fetch from "isomorphic-unfetch";
+import Markdown from "react-markdown";
 
 const Post = props => (
   <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-    {props.show.image ? <img src={props.show.image.medium} /> : null}
+    <div className='markdown'>
+      <h1>{props.show.name}</h1>
+      <p>{props.show.summary.replace(/<[/]?[pb]>/g, "")}</p>
+      {props.show.image ? <img src={props.show.image.medium} /> : null}
+    </div>
+    <style jsx global>{`
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
 );
 
